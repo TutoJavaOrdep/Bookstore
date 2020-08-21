3c371e81a6cac5684c275b775e1a40bc69238418
pepe <pedroctbr@gmail.com>
2020-08-21 14:49:36 -0500

    create user

--- Bookstore/sql/Bookstore.sql
+++ Bookstore/sql/Bookstore.sql
@@ -17,4 +17,48 @@ CREATE TABLESPACE tbsAminExam
    DATAFILE 'tbsAminExam.dbf'
    SIZE 20m;
    
-   
+  
+  
+  
+  
+CREATE TABLE book (
+ BOOK_ID NUMBER(11,0) NOT NULL,
+ TITLE VARCHAR(128) NOT NULL,
+ AUTHOR VARCHAR(45) NOT NULL,
+ PRICE FLOAT NOT NULL,
+ PRIMARY KEY (BOOK_ID),
+ CONSTRAINT  TITLE_UNIQUE UNIQUE (TITLE)
+)TABLESPACE tbsAminExam;
+
+
+
+
+
+ CREATE USER AdminExam
+    IDENTIFIED BY 12345678
+    DEFAULT TABLESPACE tbsAminExam
+    QUOTA UNLIMITED ON tbsAminExam
+    ACCOUNT LOCK;
+
+SELECT 
+    username, 
+    default_tablespace, 
+    profile, 
+    authentication_type
+FROM
+    dba_users;
+	
+	
+
+SELECT 
+    username, 
+    default_tablespace, 
+    profile, 
+    authentication_type
+FROM
+    dba_users
+WHERE 
+    account_status = 'OPEN';
+	
+	
+	
